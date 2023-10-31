e605ac986212a02dd79b18b5f0255f566636ffe8
miheich1911 <143932457+miheich1911@users.noreply.github.com>
2023-10-31 14:28:13 +0300

    внутренняя логика.py

--- морской бой/внутренняя логика.py
+++ морской бой/внутренняя логика.py
@@ -1,6 +1,4 @@
 class Dot:
-    
-
     def __init__(self, x, y):
         self.x = x          # столбец
         self.y = y          # строка
@@ -11,8 +9,6 @@ class Dot:
         return False
 
 class Ship:
-    
-
     def __init__(self, size, nose, direction):
         self.size = size
         self.nose = nose
@@ -30,57 +26,47 @@ class Ship:
         return ship_dots
 
 class Board():
-    def __init__(self, state_cells, list_ships, hid, num_sur):
-        self.state_cells = [['0'] * 6 for i in range(6)]   # список с состоянием каждой клетки
-        self.list_ships = []                               # список кораблей
-        self.hid = hid                                     # типа bool, нужно скрывать доску(для врага) или нет(для себя)
-        self.num_sur = num_sur                             # количество живых кораблей
+    def __init__(self, hid):
+        self.hid = hid                                # типа bool, нужно скрывать доску(для врага) или нет(для себя)
 
-    @staticmethod
-    def add_ships(self):
-        for dot in Ship.dots():
+    def add_ships(self, ship):
+        list_ships = []                               # список кораблей
+        num_sur = 0                                   # количество живых кораблей
+        for dot in ship.dots():
+            if dot.out() is True or dot in ship.contour() or dot in ship.dots():
+                raise Exception("Невозможно поставить корабль на это место")
+            else:
+                continue
+        list_ships.append(ship)
+        num_sur += 1
+        return list_ships
 
-
-    @staticmethod
-    def contour(self):
+    def contour(self, ship):
         contour_ship = []
-        if self.direction == 'H':
-            for i in range(-1, Ship.size):
-                for j in range(-1, 1, 2):
-                    contour_dot = Dot(Ship.nose.x + i, Ship.nose.y + j)
-                    contour_ship.append(contour_dot)
-            for i in range(Ship.size):
-                dot_ship = Dot(Ship.nose.x + i, Ship.nose.y)
-                contour_ship.remove(dot_ship)
-            return contour_ship
-        else:
-            for i in range(-1, 1, 2):
-                for j in range(-1, Ship.size):
-                    contour_dot = Dot(Ship.nose.x + i, Ship.nose.y + j)
-                    contour_ship.append(contour_dot)
-            for i in range(Ship.size):
-                dot_ship = Dot(Ship.nose.x, Ship.nose.y + i)
-                contour_ship.remove(dot_ship)
-            return contour_ship
+        for dot in ship.dots():
+            for i in range(-1, 1):
+                for j in range(-1, 1):
+                    contour_dot = dot(x + i, y + j)
+                    if contour_dot.out() is True or contour_dot in ship.dots():
+                        continue
+                    else:
+                        contour_ship.append(contour_dot)
+        return contour_ship
 
-    @staticmethod
-    def out():
-        if Dot.x < 1 or Dot.y < 1 or Dot.x > 6 or Dot.y > 6:
+    def out(self, dot):
+        if dot.x < 1 or dot.y < 1 or dot.x > 6 or dot.y > 6:
             return True
         return False
 
-    @staticmethod
-    def player_shot(self):
+    def shot(self, dot):
         try:
-            shot = Dot(x = int(input('Введите координату X выстрела:')), y = int(input('Введите координату Y выстрела:')))
-            if shot == '■':
-                shot = 'X'
+            shot_dot = dot(x = int(input('Введите координату X выстрела:')), y = int(input('Введите координату Y выстрела:')))
+            if shot_dot == '■':
+                shot_dot = 'X'
             else:
-                shot = 'T'
+                shot_dot = 'T'
         except IndexError:
             print('Вы выстрелили за пределы доски!')
-
-
 
 
     def print_board(self):
