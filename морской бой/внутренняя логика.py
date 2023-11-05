eaf1844042bd468a147ef62594a3385cdfc9fc7d
miheich1911 <143932457+miheich1911@users.noreply.github.com>
2023-11-05 10:07:11 +0300

    внутренняя логика.py

--- морской бой/внутренняя логика.py
+++ морской бой/внутренняя логика.py
@@ -34,12 +34,12 @@ class Board():
         self.occupied_points = []
 
     def add_ship(self, ship):
-        for dot in ship.dots():
+        for dot in ship.dots:
             if self.out(dot) or dot in self.occupied_points:
                 raise Exception("Невозможно поставить корабль на это место!")
-        for dot in ship.dots():
+        for dot in ship.dots:
             self.field[dot.x - 1][dot.y - 1] = '■'
-        self.occupied_points.append(ship.dots())
+        self.occupied_points.append(ship.dots)
         self.occupied_points.append(self.contour(ship))
         self.list_ships.append(ship)
         self.num_sur += 1
@@ -47,11 +47,11 @@ class Board():
 
     def contour(self, ship):
         contour_ship = []
-        for dot in ship.dots():
+        for dot in ship.dots:
             for i in range(-1, 1):
                 for j in range(-1, 1):
                     contour_dot = Dot(dot.x + i, dot.y + j)
-                    if self.out(contour_dot) or contour_dot in ship.dots():
+                    if self.out(contour_dot) or contour_dot in ship.dots:
                         continue
                     else:
                         contour_ship.append(contour_dot)
@@ -62,28 +62,35 @@ class Board():
             return True
         return False
 
-    def shot(self, dot, ship):
-        shot_dot = dot(int(input('Введите номер строки для выстрела:')), int(input('Введите номер колонны для выстрела:')))
-        try:
-            if shot_dot == '■':
-                shot_dot = 'X'
-                print('Попал!')
-                ship.hp -= 1
-            else:
-                shot_dot = 'T'
-                print('Мимо!')
-            if ship.hp == 0:
-                for dot in ship.contour():
-                    dot = 'T'
-                print('Убит!')
-        except IndexError:
-            print('Вы выстрелили за пределы доски!')
-
+    def shot(self, dot):
+        perfect_shots = []
+        if dot in perfect_shots:     #если выстрел В СПИСКЕ произведенных выстрелов
+            raise Exception("Вы уже стреляли в эту клетку! Введите корректные данные для выстрела!")
+        else:     #если выстрел НЕ В СПИСКЕ произведенных выстрелов
+            for ship in self.list_ships:     #для корябля из списка кораблей
+                if dot in ship.dots:     #если выстрел ПОПАДАЕТ в одну из точек корабля
+                    self.field[dot.x - 1][dot.y - 1] = 'X'     #точка помечается знаком 'X'
+                    ship.hp -= 1     #жизнь корабля уменьшается на 1
+                    perfect_shots.append(dot)     #выстрел записывается в список произведенных выстрелов\
+                    print('Попал! Сделайте еще один выстрел')
+                    if ship.hp == 0:     #если корабль погибает
+                        for dot in self.contour(ship):     # для точек контура корабля
+                            self.field[dot.x - 1][dot.y - 1] = 'T'      #точки помечаются знаком 'T'
+                            self.num_sur -= 1     #счетчик живых кораблей уменьшается на 1
+                            self.list_ships.remove(ship)     #корабль удалаяется из списка кораблей
+                            perfect_shots.append(dot)     #точки контура добавляются в список не разрешенных для выстрела клеток. то есть в список произведенных выстрелов, чтобы игрок по ним уже не мог стрелять
+                            print('Корабль потоплен!') 
+                    return perfect_shots
+                else:    #если выстрел НЕ ПОПАДАЕТ в одну из точек корабля
+                    if dot in self.out(dot):    #если выстрел ВЫШЕЛ за пределы поля
+                        raise Exception("Введите корректные данные для выстрела!")
+                    else:    #если выстрел НЕ ВЫШЕЛ за пределы поля (но он не попал в корабль, то есть промах)
+                        self.field[dot.x - 1][dot.y - 1] = 'T'    #точка помечается знаком 'X'
+                        perfect_shots.append(dot)
+                        print('Промах!')
+                    return perfect_shots
 
     def print_board(self):
         print(" |1|2|3|4|5|6")
         for i, row in enumerate(self.field, start=1):
             print(i, *row, sep='|')
-
-
-
