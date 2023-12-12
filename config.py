d34849b23a138a7fa9300281adfe4b2897c4def2
Дмитрий Михеев <143932457+miheich1911@users.noreply.github.com>
2023-12-12 13:02:31 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = '6981984007:AAGZ9lx8bldk239UMzOD2UVwFveUCtf2C0U'
+
+keys = {
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль': 'RUB'
+}
---
+++ extensions.py
@@ -0,0 +1,34 @@
+import requests
+import json
+from config import keys
+
+
+
+class APIException(Exception):
+    pass
+
+
+class CurrencyConverter:
+    @staticmethod
+    def get_price(quote: str, base: str, amount: str):
+        if quote == base:
+            raise APIException(f'Невозможно перевести одинаковые валюты {base}.')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise APIException(f'Введите корректно валюту {quote}')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise APIException(f'Введите корректно валюту {base}')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Не удалось обработать количество {amount}')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = json.loads(r.content)[keys[base]] * amount
+        return total_base
